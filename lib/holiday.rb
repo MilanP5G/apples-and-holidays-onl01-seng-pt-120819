e4a7965d20022f248c6e3da18f174710a100815f
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-19 21:16:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,8 +96,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.collect do |season, time|
-    time.collect do |day, supply|
+  holiday_hash.map do |season, time|
+    time.map do |day, supply|
       if supply.include? "BBQ"
         day
       end 
